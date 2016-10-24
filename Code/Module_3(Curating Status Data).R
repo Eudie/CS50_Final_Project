1fbb653aca7bc66819d90cef02e7b6c90b5aefaf
Eudie <utkarsh.dubey.26@gmail.com>
2016-10-24 08:04:59 +0530

    made descreate delay

--- Code/Module_3(Curating Status Data).R
+++ Code/Module_3(Curating Status Data).R
@@ -27,18 +27,37 @@ your_data_frame <- do.call(rbind,lapply(paste0("Data/Daily_status/TestDir/",file
 your_data_frame$uniqueKey <- paste(your_data_frame$train_no, your_data_frame$station, your_data_frame$scharr_datetime, sep = "|")
 length(unique(your_data_frame$uniqueKey))
 uniquedf <- your_data_frame[!(duplicated(your_data_frame$uniqueKey)),]
-uniquedf$scharr_datetime <- as.POSIXct(uniquedf$scharr_datetime, "%Y-%m-%d %H:%M:%S")
-uniquedf$schdep_datetime <- as.POSIXct(uniquedf$schdep_datetime, "%Y-%m-%d %H:%M:%S")
-uniquedf$actarr_datetime <- as.POSIXct(uniquedf$actarr_datetime, "%Y-%m-%d %H:%M:%S")
-uniquedf$actdep_datetime <- as.POSIXct(uniquedf$actdep_datetime, "%Y-%m-%d %H:%M:%S")
+uniquedf$scharr_datetime <- as.POSIXct(uniquedf$scharr_datetime,tz= "India", "%Y-%m-%d %H:%M:%S")
+uniquedf$schdep_datetime <- as.POSIXct(uniquedf$schdep_datetime,tz= "India", "%Y-%m-%d %H:%M:%S")
+uniquedf$actarr_datetime <- as.POSIXct(uniquedf$actarr_datetime,tz= "India", "%Y-%m-%d %H:%M:%S")
+uniquedf$actdep_datetime <- as.POSIXct(uniquedf$actdep_datetime,tz= "India", "%Y-%m-%d %H:%M:%S")
 
 onlyReachedDF <- uniquedf[uniquedf$is_reached == TRUE,]
 summary(onlyReachedDF$scharr_datetime)
 crowd <- integer()
 for(x in onlyReachedDF$uniqueKey){
   fil <- onlyReachedDF[onlyReachedDF$uniqueKey == x,]
-  uniquedf$crowd[uniquedf$uniqueKey == x] <- nrow(onlyReachedDF[(onlyReachedDF$station == fil$station &
+  onlyReachedDF$crowd[onlyReachedDF$uniqueKey == x] <- nrow(onlyReachedDF[(onlyReachedDF$station == fil$station &
                                                onlyReachedDF$actarr_datetime >= (fil$actarr_datetime - hours(1)) &
                                                onlyReachedDF$actarr_datetime <= (fil$actarr_datetime + hours(1))),])
 }
-uniquedf$crowd[is.na(uniquedf$crowd)] <- 0
+onlyReachedDF$crowd[is.na(uniquedf$crowd)] <- 0
+
+for(i in c(1:length(onlyReachedDF$scharr_datetime))){
+  if(onlyReachedDF$no[i]==1){
+    onlyReachedDF$oneRoute[i] = TRUE
+  }else if((onlyReachedDF$scharr_datetime[i]- onlyReachedDF$scharr_datetime[i-1])>0 & onlyReachedDF$oneRoute[i-1]==TRUE){
+    onlyReachedDF$oneRoute[i] = TRUE
+  }else{
+    onlyReachedDF$oneRoute[i] = FALSE
+  }
+}
+
+df1 <- onlyReachedDF[onlyReachedDF$oneRoute == T,]
+ for(j in c(1:length(df1$scharr_datetime))){
+   if(df1$no[j]==1){
+     df1$lateDiscreate[j] = df1$latemin[j]
+   }else{
+     df1$lateDiscreate[j] = df1$latemin[j] - df1$latemin[j-1]
+   }
+ }
